a5fc162f8639f1c9b35ab8895c37c02b95b0224d
Nikolay Feldman <niko@nfeld.com>
2022-12-25 02:22:40 -0500

    setcookie api

--- src/pages/api/headers.js
+++ src/pages/api/headers.js
@@ -1,3 +1,5 @@
 export default async function handler(req, res) {
+    console.log('/api/headers Request Headers: ', req.headers)
+
     res.status(200).json(req.headers)
 }
---
+++ src/pages/api/setcookie.js
@@ -0,0 +1,46 @@
+import Cookies from 'cookies'
+
+export default async function handler(req, res) {
+    if (req.method !== 'GET') {
+        res.status(404).end()
+        return
+    }
+
+    console.log('/api/setcookie Request Headers: ', req.headers)
+
+    const cookies = new Cookies(req, res)
+    const cookieNames = Object.keys(req.query)
+    
+    const httpOnly = !!cookieNames.find(it => it.toLowerCase() === 'httponly')
+    const deleteCookie = req.query.delete
+    if (deleteCookie) {
+        if (Array.isArray(deleteCookie)) {
+            deleteCookie.forEach(it => cookies.set(it))
+        } else {
+            cookies.set(deleteCookie)
+        }
+    }
+
+    cookieNames.forEach(cookie => {
+        // ignore certain names
+        if (cookie.toLowerCase() === 'delete' || cookie.toLowerCase() === 'httponly') {
+            return
+        }
+
+        const value = req.query[cookie]
+        const isArrayValue = Array.isArray(value)
+
+        if (isArrayValue) {
+            return // ignore duplicate cookie names
+        }
+        cookies.set(cookie, value, {
+            domain: '.nfeld.com',
+            path: '/',
+            maxAge: 600000,
+            httpOnly,
+        })
+    })
+
+
+    res.status(200).json(req.headers)
+}
